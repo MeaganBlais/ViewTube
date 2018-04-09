8cf87e6ac8dca01c82fb860bfca38f2bb8af6577
Meagan Blais <meaganblais@gmail.com>
2018-04-09 15:04:10 -0400

    adding first component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//create a new class/type of component used to produce HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// use jsx tags around the component name to create instance
+// take component's generated HTML and put it in the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
