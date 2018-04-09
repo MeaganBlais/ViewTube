631a538c8f6a1e81e3ca95a3ad2f510008c0500d
Meagan Blais <meaganblais@gmail.com>
2018-04-09 17:28:29 -0400

    setting up our constructor & updating event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,8 +6,19 @@ import React, { Component } from 'react';
 // required: define the render method for the class
 // required: return jsx
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value) } />
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
 }
 
