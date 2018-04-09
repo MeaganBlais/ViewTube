148e4ab541df67df6a45669a5443e3cb49b70eed
Meagan Blais <meaganblais@gmail.com>
2018-04-09 17:52:02 -0400

    setting up youtube search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDM_UzMxSV2qA2rB_TEGPMM6QXrjq-6PTg';
+
+YTSearch({key: API_KEY, term: 'puppies'}, function(data) {
+  console.log(data);
+});
 
 // create a function component used to produce HTML
 const App = () => {
