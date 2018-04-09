7800e960e9d05f9375dba2fb7be7f34a43bf71e0
Meagan Blais <meaganblais@gmail.com>
2018-04-09 16:01:58 -0400

    setting up search_bar component

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// declare class-based component
+// required: define the render method for the class
+// required: return jsx
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDM_UzMxSV2qA2rB_TEGPMM6QXrjq-6PTg';
 
-//create a new class/type of component used to produce HTML
-
+// create a function component used to produce HTML
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // use jsx tags around the component name to create instance
