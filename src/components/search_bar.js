e8d5c0c468a40436ef1314a38ad66c4ac46c48a2
Meagan Blais <meaganblais@gmail.com>
2018-04-09 16:06:37 -0400

    cleaning up code

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+// { Component } means import React and pull of the porperty Component 
+// same as declaring Component = React.Component
+import React, { Component } from 'react';
 
-// declare class-based component
+// declare class-based component and add functionality by extending Component
 // required: define the render method for the class
 // required: return jsx
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render() {
     return <input />;
   }
