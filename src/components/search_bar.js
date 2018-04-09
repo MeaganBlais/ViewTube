74917101f02b002d239c1e80b2ef1bf12af4b4af
Meagan Blais <meaganblais@gmail.com>
2018-04-09 16:31:11 -0400

    adding event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,7 +7,13 @@ import React, { Component } from 'react';
 // required: return jsx
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={this.onInputChange} />;
+  }
+
+  // event handler naming convention is to use 'handle' OR 'on', the element 
+  // you're watching for event on & the name of the event
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
